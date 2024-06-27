da1b00c84d2d9b45908d447355467cae9dd14705
malekoa <azadeganmalek@gmail.com>
2024-06-26 21:40:17 -0700

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set your repository variables
+REPO="https://github.com/malekoa/malekoa.github.io.git"
+BRANCH="main" # or "master"
+
+# Build the Hugo site
+hugo
+
+# Go to the public directory
+cd public
+
+# Initialize a new git repository and push the files
+git init
+git add .
+git commit -m "Deploying site"
+git remote add origin $REPO
+git push -f origin $BRANCH
